8595bf7764dab7a9abdc19494a868c74b072213a
jeremylindsayni <jeremy.lindsay@outlook.com>
2017-03-19 11:52:13 +0000

    Initial commit.

---
+++ RaspberryPiCore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RaspberryPiCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Internet of Things!");
+        }
+    }
+}
